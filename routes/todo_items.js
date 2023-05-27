0bdef80be33160478a511e4b629785472ce93d62
nagaraj6618 <nagaraj516700@gmail.com>
2023-05-27 11:02:59 +0530

    Updated MVC to get  method

--- routes/todo_items.js
+++ routes/todo_items.js
@@ -2,5 +2,5 @@ const express=require('express')
 const router=express.Router()
 
 const {getALLToDoItems}=require('../controllers/todo_items')
-router.route("/").get(getALLToDoItems)
+router.route("/").get(getALLToDoItems);
 module.exports=router
