0b719f5d624d497131874b529a7c676b9eb3f045
nagaraj6618 <nagaraj516700@gmail.com>
2023-05-27 10:39:31 +0530

    todo_items.ja created as router

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("App.js Page");
+const express=require('express')
+const app=express();
+const PORT = 4500;
+const todo_items= require('./routes/todo_items.js')
+
+app.get('/',(req,res)=>{
+  res.send("Working");
+})
+app.use('/api/v1',todo_items)
+app.listen(PORT,console.log(`Server runs at http://localhost:${PORT}`));
